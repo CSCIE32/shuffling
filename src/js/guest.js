aaa2f4f1decc4ed628ba0348eff940daa434bd69
Selvam, Nishanth <nselvam@akamai.com>
2015-10-31 11:17:26 -0400

    Initial Commit

--- src/js/guest.js
+++ src/js/guest.js
@@ -1,4 +1,4 @@
-app.module('shuffling').factory('Guest', function(){
+angular.module('shuffling').factory('Guest', function(){
     var Guest = function(name, transitionDate, pickupLocation){
         this.name = name;
         this.transitionDate = transitionDate;
